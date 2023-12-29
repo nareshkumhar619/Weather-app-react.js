23808bbf229b9f73e3e8a6fe1f16f5d8c7e6ca92
Naresh Kumhar <122338149+nareshkumhar619@users.noreply.github.com>
2023-12-29 11:02:43 +0530

    updated

--- README.md
+++ README.md
@@ -1,4 +1,50 @@
-# React + Vite
+# Fusiontecz Solution Assignment
 
-This is a Weather - App. Created in React js with Real-Time Data fetching.<br/>
-Basically, The project contains the concept of Hooks, Api-Fetching & Dynamic data implementation.
+## Weather Forecast App
+
+Welcome to the Weather Forecast Application, a React-based web application that provides real-time weather forecasts for multiple cities. This application leverages the OpenWeather API to fetch accurate and up-to-date weather data. Users can easily search for cities, add them to their list, and view detailed weather forecasts, including hourly updates.
+
+## Features
+
+- **City Search**: Users can search for cities worldwide and add them to their personalized weather forecast list.
+- **Multiple Cities**: The application supports tracking the weather for multiple cities simultaneously.
+- **Current Weather**: Get the latest weather conditions for each city, including temperature, humidity, wind speed, and more.
+- **Hourly Forecast**: View detailed hourly weather forecasts to plan your activities throughout the day.
+- **Responsive Design**: The application is designed to work seamlessly across various devices, providing a consistent user experience.
+
+## Installation
+
+Clone this repository in your local machine and install the needed dependencies.
+
+For more understanding, so to OpenWeatherAPI for documentations. 
+
+Type **npm start** in terminal to start the application after installing the dependencies using **npm install**
+
+## How to Use
+- **Search for Cities:** Use the search bar to find and add cities to your forecast list.
+- **Track Cities:** Easily track cities by clicking back and next button.
+- **Current Weather:** View the current weather conditions for each city.
+- **Hourly Forecast:** Check the hourly forecast to plan your day effectively.
+
+## Application Folder Structure
+
+```
+├── public/
+│	├── index.html
+│    	├──images/	
+├──src/
+│	├──App.js
+│	├──App.css
+│	├──App.test.js
+│	├──index.css
+│	├──index.js
+│	├──Components/
+│		├──CityInputComponent.js
+│		├──CityInputComponent.css
+│		├──HourlyForecast.js
+│		├──HourlyForecast.css
+│		├──WeatherDetailsComponent.js
+│		├──WeatherDetailsComponent.css  
+├──package-lock.json
+├──package.json
+```
---
+++ src/App.js
@@ -0,0 +1,86 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import CityInput from './components/CityInput';
+import WeatherDetails from './components/WeatherDetails';
+import WeatherForecast from './components/HourlyForecast';
+import "./App.css";
+
+
+const App = () => {
+  const [cities, setCities] = useState([]);
+  const [currentCityIndex, setCurrentCityIndex] = useState(0);
+  const [weatherData, setWeatherData] = useState(null);
+  const [showRecords, setShowRecords] = useState(false);
+  const apiKey = "6efea12c32f48c1ba383c739d2907c50"; 
+
+  useEffect(() => {
+    if (cities.length > 0) {
+      const currentCity = cities[currentCityIndex];
+      axios
+        .get(`https://api.openweathermap.org/data/2.5/weather?q=${currentCity}&appid=${apiKey}&units=metric`)
+        .then((response) => {
+          console.log(response.data);
+          setWeatherData(response.data)
+        })
+        .catch((error) => console.error('Error fetching weather data', error));
+    }
+  }, [cities, currentCityIndex, apiKey]);
+
+  const handleAddCity = (city) => {
+    setCities([...cities, city]);
+    setCurrentCityIndex(cities.length);
+  };
+  console.log(cities)
+  const handleNextCity = () => {
+    setCurrentCityIndex((prevIndex) => (prevIndex + 1) % cities.length);
+  };
+
+  const handlePrevCity = () => {
+    setCurrentCityIndex((prevIndex) => (prevIndex - 1 + cities.length) % cities.length);
+  };
+
+  const handleToggleRecords = () => {
+    setShowRecords((prevShowRecords) => !prevShowRecords);
+  };
+
+  return (
+    <div>
+      <div className="main-container">
+        <div className="left-container">
+          <CityInput onAddCity={handleAddCity}/>
+          <div className='city-change-btn'>
+          <button className='btn'  onClick={handlePrevCity}>Back</button>
+          <button className='btn' onClick={handleNextCity}>Next</button>
+          </div> 
+          {cities.length > 0 && weatherData && (
+            <div className="city-info">
+              <h2>{weatherData?.name},{weatherData?.sys?.country}</h2>
+              <div className="temperature">{weatherData?.main?.temp}°C</div>
+              <h2>{weatherData?.weather[0]?.main}</h2>
+            </div>
+          )}
+            <img className='city-image' alt='city image' src={process.env.PUBLIC_URL + '/images/cityImage.jpg'} />
+        </div>
+        <div className="right-container">
+          <h2>Welcome back Isabella!</h2>
+          <h4>Check out Hourly Weather Forecast</h4>
+          <WeatherForecast selectedCity={cities[currentCityIndex]} />
+          <h4>More Details of today's weather</h4>
+          <div className='weather-details-card'>
+            <WeatherDetails city={cities[currentCityIndex]} weatherData={weatherData} /> 
+          </div>
+          <div className="show-records">
+            <button className='btn-records' onClick={handleToggleRecords}><strong>Show Records</strong></button>
+            {showRecords && (
+              <div className='records-font' >
+                <h4>Added Cities: {cities.map(city => city.charAt(0).toUpperCase() + city.slice(1)).join(', ')}</h4>
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
